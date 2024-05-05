e935d4fb6528d9b748a8be89b1ad07a694005652
Himanshu Singh <himansh.singh293@gmail.com>
2024-05-05 09:40:57 +0530

    Created searchBar component

---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import  "./SearchBar";
+
+export default function SearchBar() {
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const handleSearchInputChange = (event) => {
+      setSearchQuery(event.target.value);
+      // You can perform search logic here
+
+    };
+  
+    return (
+        <div className="search-container">
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={handleSearchInputChange}
+          className="search-input"
+          placeholder="Search..."
+        />
+        <button className="search-button">Search</button>
+      </div>
+    );
+}
