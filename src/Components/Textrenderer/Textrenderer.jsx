1e6293e76c86391e4565a90b0942fd3c8cfc869e
Himanshu Singh <himansh.singh293@gmail.com>
2024-05-07 16:18:02 +0530

    refactor textrenderer component code

--- src/Components/Textrenderer/Textrenderer.jsx
+++ src/Components/Textrenderer/Textrenderer.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import parse from 'html-react-parser';
+import parse from "html-react-parser";
 
 export default function Textrenderer({ text, searchTerm }) {
   const [textToRender, setTextToRender] = useState(text);
@@ -7,34 +7,37 @@ export default function Textrenderer({ text, searchTerm }) {
   const [totalSearchTermCount, setTotalSearchTermCount] = useState(0);
 
   const processText = (text, searchTerm) => {
-
     const regex = new RegExp(`(${searchTerm})`, "gi"); // Case insensitive search
     const matches = text.match(regex) || [];
     const count = matches.length;
     const highlightedText = text.replace(
       regex,
-      `<span class="highlight">$1</span>`
+      `<span class="highlight">$1</span>`,
     );
-    setTotalSearchTermCount(count);
-    setTotalWordCount(text.split(/\s+/).filter(word => word !== "").length)
+    searchTerm.length > 0
+      ? setTotalSearchTermCount(count)
+      : setTotalSearchTermCount(0);
+
+    setTotalWordCount(text.split(/\s+/).filter((word) => word !== "").length);
     return parse(highlightedText);
   };
 
-  useEffect(()=>{
-
-    setTextToRender(processText(text ,searchTerm));
-  },[text, searchTerm])
-
+  useEffect(() => {
+    setTextToRender(processText(text, searchTerm));
+  }, [text, searchTerm]);
 
   return (
     <>
-      {text.length > 0 ? <div>
-         <div>{textToRender}</div>
-         <div>{totalSearchTermCount}</div>
-         <div>{totalWordCount}</div>
-         </div> : <div>Please input file</div>
-      }
-     
+      {text.length > 0 ? (
+        <div className="flex flex-col">
+          <div className="text-gray-400  pr-6 mb-2 ">
+            Word Found: {totalSearchTermCount} / Total Words: {totalWordCount}
+          </div>
+          <div className="text-lg ">{textToRender}</div>
+        </div>
+      ) : (
+        <></>
+      )}
     </>
   );
 }
