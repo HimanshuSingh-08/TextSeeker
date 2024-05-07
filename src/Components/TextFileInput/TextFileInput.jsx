212c7eff4edae8d0f9090b819f534806b20c5cf1
Himanshu Singh <himansh.singh293@gmail.com>
2024-05-07 16:16:41 +0530

    refactor textfileinput component code

--- src/Components/TextFileInput/TextFileInput.jsx
+++ src/Components/TextFileInput/TextFileInput.jsx
@@ -1,32 +1,58 @@
+import { useEffect, useRef } from "react";
 
-export default function TextFileInput({setTextFileContent}) {
+export default function TextFileInput({ setTextFileContent }) {
+  const inputFileRef = useRef(null);
 
-    
   const handleFileChange = (event) => {
     const file = event.target.files[0];
+    const allowedExtensions = /\.txt$/i;
     if (!file) {
-        setTextFileContent('');
+      setTextFileContent("");
+      return;
+    }
+    // only allow .txt files to be uploaded
+    if (!allowedExtensions.exec(file.name)) {
+      alert("Invalid file type. Please upload a.txt file.");
+      setTextFileContent("");
       return;
     }
 
     const reader = new FileReader();
     reader.onload = (e) => {
-        setTextFileContent(e.target.result);
+      setTextFileContent(e.target.result);
     };
-    reader.readAsText(file)
-
+    reader.readAsText(file);
   };
 
-  return (
+  const handleKeyDown = (e) => {
+    if (
+      e.keyCode === 85 &&
+      (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)
+    ) {
+      e.preventDefault();
+      console.log("click shortcut");
+      inputFileRef.current.click();
+    }
+  };
 
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown, false);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  });
+
+  return (
     <div>
-         <input
-          type="file"
-          name="text-file"
-          id="txt"
-          className="file-input"
-          onChange={handleFileChange}
-        />
+      <input
+        type="file"
+        ref={inputFileRef}
+        name="text-file"
+        id="txt"
+        className="mb-6 cursor-pointer"
+        onChange={handleFileChange}
+      />
     </div>
-  )
+  );
 }
