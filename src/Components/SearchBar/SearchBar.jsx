d25be8aa597f371cf2cd6bea70287129b71833cd
Himanshu Singh <himansh.singh293@gmail.com>
2024-05-07 16:12:10 +0530

    refactor searchbar code

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function SearchBar({
   setSearchTerm,
@@ -6,19 +6,54 @@ export default function SearchBar({
   setSearchHistory,
 }) {
   const [searchQuery, setSearchQuery] = useState(searchTerm);
+  const searchRef = useRef(null);
+
+  const handleSumbit = () => {
+    setSearchHistory((prevHistory) => {
+      return [searchQuery, ...prevHistory];
+    });
+    setSearchTerm(searchQuery);
+  };
+
+  // Shift + f to focus on input box.
+  const handleKeyDown = (e) => {
+    if (
+      e.keyCode === 70 &&
+      (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)
+    ) {
+      e.preventDefault();
+      searchRef.current.focus();
+    }
+  };
+
+  useEffect(() => {
+    setSearchQuery(searchTerm);
+    window.addEventListener("keydown", handleKeyDown, false);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [searchTerm]);
+
   return (
     <>
-      <div>SearchBar</div>
       <input
         type="text"
+        ref={searchRef}
         value={searchQuery}
         onChange={(e) => {
           setSearchQuery(e.target.value);
         }}
-        className="search-input"
+        onKeyDown={(e) => {
+          if (e.key == "Enter") {
+            handleSumbit();
+          }
+        }}
+        className=" border-1 border-gray-400 rounded-md p-2 mr-2"
         placeholder="Search..."
       />
       <button
+        className="border-gray-500 border-1  p-2 rounded-md"
         onClick={() => {
           setSearchHistory((prevHistory) => {
             return [searchQuery, ...prevHistory];
@@ -26,7 +61,7 @@ export default function SearchBar({
           setSearchTerm(searchQuery);
         }}
       >
-        Search
+        🔍
       </button>
     </>
   );
